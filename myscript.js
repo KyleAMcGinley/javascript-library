7c851ea562eca4d7f4869f90854cd3a2ef87068f
Kyle McGinley <kyleamcginley@gmail.com>
2022-09-20 20:14:33 -0400

    Created constructor
    Created function that adds new book to library
    
    Will work on html to create form

--- myscript.js
+++ myscript.js
@@ -1,12 +1,19 @@
 let myLibrary = [];
 
+//This function creates a book object with the title, author, pages, and read status
 function book(title, author, pages, read){
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
 }
-
-function addBookToLibrary(){
-    
+//Need to make an add book function that receives user input and stores new object into myLibrary array
+function addBookToLibrary(newBook){
+    return myLibrary.push(newBook);
 }
+
+const harryPotter = new book('Harry Potter', 'J.K. Rowling', 1000, 'read');
+
+addBookToLibrary(harryPotter);
+
+console.log(myLibrary);
